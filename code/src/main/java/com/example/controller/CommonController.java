a63a3abe2b4e6fd9e0165d7644cad2cea0d481a0
李文龙 <wenlongup@163.com>
2022-03-29 16:10:18 +0800

    888

---
+++ code/src/main/java/com/example/controller/CommonController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import com.example.base.BaseVO;
+import com.example.controller.vo.SubReqVO;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author: 李文龙
+ * @create: 2019-07-25 17:38
+ * @description:
+ **/
+@RestController
+@RequestMapping("api/common")
+public class CommonController {
+
+    @PostMapping("sub")
+    public BaseVO<String> sub(@RequestBody SubReqVO vo) {
+
+        System.out.println(vo.getName() + vo.getCompanyName() + vo.getPhone() + vo.getDemand());
+
+        return BaseVO.build200();
+    }
+}
+
---
+++ code/src/main/java/com/example/controller/vo/SubReqVO.java
@@ -0,0 +1,16 @@
+package com.example.controller.vo;
+
+import lombok.Data;
+
+/**
+ * @author 李文龙
+ * @date 2021-11-24
+ * @description 描述
+ */
+@Data
+public class SubReqVO {
+    private String name;
+    private String companyName;
+    private String phone;
+    private String demand;
+}
